00723ab8ae0e3a37ead4b13350a66919744d853e
Kris Lee <baocn.101@icloud.com>
2019-09-06 11:13:18 -0500

    Added class that can send telemetry to the visualizer.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ComputerDebugging.java
@@ -0,0 +1,171 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import org.firstinspires.ftc.teamcode.Vehicle;
+import org.firstinspires.ftc.teamcode.PVector;
+
+import java.text.DecimalFormat;
+
+public class ComputerDebugging
+{
+    //this is what actually sends our messages
+    private static UdpServer udpServer;
+    //this is what we will use to build the messages
+    private static StringBuilder messageBuilder = new StringBuilder();
+
+    //use this to format decimals
+    private static DecimalFormat df = new DecimalFormat("#.00");
+
+    /**
+     * Initializes udp server and starts it's thread
+     */
+    public ComputerDebugging()
+	{
+        UdpServer.kill = false;
+        udpServer = new UdpServer(11115);
+        Thread runner = new Thread(udpServer);
+        runner.start();//go go go
+    }
+
+
+
+    /**
+     * Sends the robot location to the debug computer
+     */
+    public static void sendRobotLocation(Vehicle vehicle)
+	{
+        //return if not using computer
+//        if(!Robot.usingComputer){return;}
+
+        //first send the robot location
+        messageBuilder.append("ROBOT,");
+        messageBuilder.append(df.format(vehicle.position.x));
+        messageBuilder.append(",");
+        messageBuilder.append(df.format(vehicle.position.y));
+        messageBuilder.append(",");
+        messageBuilder.append(df.format(vehicle.position.y)); // REPLACE with HEADING
+        messageBuilder.append("%");
+
+    }
+
+    /**
+     * Sends the robot location to the debug computer
+     */
+//    public static void sendRobotLocation(Vehicle car){
+//        //return if not using computer
+//        if(!Robot.usingComputer){return;}
+//
+//        //first send the robot location
+//        messageBuilder.append("CAR,");
+//        messageBuilder.append(df.format(car.getX()));
+//        messageBuilder.append(",");
+//        messageBuilder.append(df.format(car.getY()));
+//        messageBuilder.append(",");
+//        messageBuilder.append(df.format(car.getHeading()));
+//        messageBuilder.append("%");
+//
+//    }
+
+
+    /**
+     * Sends the location of any point you would like to send
+     * @param floatPoint the point you want to send
+     */
+    public static void sendKeyPoint(FloatPoint floatPoint)
+	{
+//        if(!Robot.usingComputer){return;}
+
+        messageBuilder.append("P,")
+                .append(df.format(floatPoint.x))
+                .append(",")
+                .append(df.format(floatPoint.y))
+                .append("%");
+    }
+
+
+    /**
+     * This is a point you don't want to clear every update
+     * @param floatPoint the point you want to send
+     */
+    public static void sendLogPoint(FloatPoint floatPoint)
+	{
+//        if(!Robot.usingComputer){return;}
+
+        messageBuilder.append("LP,")
+                .append(df.format(floatPoint.x))
+                .append(",")
+                .append(df.format(floatPoint.y))
+                .append("%");
+    }
+
+    /**
+     * This is a point you don't want to clear every update
+     * @param floatPoint the point you want to send
+     */
+    public static void sendLogPoint(PVector floatPoint)
+	{
+//        if(!Robot.usingComputer){return;}
+
+        messageBuilder.append("LP,")
+                      .append(df.format(floatPoint.x))
+                      .append(",")
+                      .append(df.format(floatPoint.y))
+                      .append("%");
+    }
+
+
+
+    /**
+     * Used for debugging lines
+     * @param point1
+     * @param point2
+     */
+    public static void sendLine(FloatPoint point1, FloatPoint point2)
+	{
+        //return if not using the computer
+//        if(!Robot.usingComputer){return;}
+        messageBuilder.append("LINE,")
+                .append(df.format(point1.x))
+                .append(",")
+                .append(df.format(point1.y))
+                .append(",")
+                .append(df.format(point2.x))
+                .append(",")
+                .append(df.format(point2.y))
+                .append("%");
+    }
+
+
+    /**
+     * This kills the udpServer background thread
+     */
+    public static void stopAll()
+	{
+//        if(!Robot.usingComputer){return;}
+
+        UdpServer.kill = true;
+    }
+
+    /**
+     * Sends the data accumulated over the update by adding it to the udpServer
+     */
+    public static void markEndOfUpdate()
+	{
+//        if(!Robot.usingComputer){return;}
+        messageBuilder.append("CLEAR,%");
+
+//        udpServer.addMessage(messageBuilder.toString());
+        udpServer.splitAndSend(messageBuilder.toString());
+        messageBuilder = new StringBuilder();
+    }
+
+    /**
+     * Forces a clear log
+     */
+    public static void clearLogPoints()
+	{
+//        if(!Robot.usingComputer){return;}
+        udpServer.splitAndSend("CLEARLOG,%");
+
+    }
+}
