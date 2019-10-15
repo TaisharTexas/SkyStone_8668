36ce0855ee0f1919d12196b10d9b53ae69b6ff9e
Kristopher A. Lee <kristopher.a.lee@nasa.gov>
2019-10-14 20:05:38 -0500

    working on Action Master to get the init method worked out.  Moved init for the action master from the autonomous class to the ActionMaster class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPursuit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPursuit.java
@@ -6,13 +6,18 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 
+import org.firstinspires.ftc.teamcode.sbfActions.ActionMaster;
+
+import java.io.File;
+
 @Autonomous(name="pursuit run", group="pure")
 @Config
 public class AutoPursuit extends OpMode
 {
+    ActionMaster theMaster = new ActionMaster();
+
     Pursuit pursuit = new Pursuit((float)0.0, (float)0.0, telemetry);
     Robot robot = new Robot();
-
     Path drivePath = new Path();
 
 
@@ -22,8 +27,12 @@ public class AutoPursuit extends OpMode
 
         robot.init(telemetry, hardwareMap, true);
 
+        File autoFile = new File("/auto.csv");
+        theMaster.init(telemetry, autoFile, robot);
+
         robot.getEncoderTelem();
 
+        // Set up path
         drivePath.addPoint(0,0,30,0);
         drivePath.addPoint(0, 48, 30, 0);
         drivePath.addPoint(48, 48, 30, 0);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sbfActions/ActionMaster.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sbfActions/ActionMaster.java
@@ -1,10 +1,16 @@
 package org.firstinspires.ftc.teamcode.sbfActions;
 
-import com.qualcomm.robotcore.robot.Robot;
 
+import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.sbfActions.RobotAction;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
@@ -31,9 +37,88 @@ public class ActionMaster
     boolean firstRun = true;
 
     /** Initializes telemetry. */
-    public void init(Telemetry telem)
+    public void init(Telemetry telem, File autoFile, Robot robot)
     {
         telemetry = telem;
+        BufferedReader br = null;
+        String line = "null";
+        try
+        {
+            RobotAction myAction;
+
+            FileReader inputStreamReader = new FileReader(autoFile);
+
+            br = new BufferedReader(inputStreamReader);
+
+            br.readLine();  // throw away first line by reading it and not looking at it
+
+            while((line = br.readLine()) != null)
+            {
+                // Get a line from the CSV file and split it on the commas
+                String[] theItems = line.split(",");
+
+                // Grab the first one... this will be the type of RobotAction to create
+                String type = theItems[0].trim().toUpperCase();
+
+                // Grab the reset of the items after the first one... these are the parameters to use
+                // with the constructors for each of the RobotAction subclasses.
+                String[] params = (Arrays.copyOfRange(theItems, 1, theItems.length));
+
+                // Based on the type, make the specific kind of RobotAction
+                if (type.equalsIgnoreCase("WAITACTION"))
+                {
+                    myAction = new WaitAction(params);
+                }
+                else if (type.equalsIgnoreCase("DRIVEACTION"))
+                {
+                    myAction = new DriveAction(params);
+                }
+                else if(type.equalsIgnoreCase("CAMERAACTION"))
+                {
+                    myAction = new CameraAction(params);
+                }
+                else if(type.equalsIgnoreCase("GYROACTION"))
+                {
+                    myAction = new GyroAction(params);
+                }
+                else
+                {
+                    myAction = null;
+                }
+
+                if(myAction != null)
+                {
+                    myAction.init(telemetry, robot);
+                    this.addAction(myAction);
+                }
+                telemetry.addData("TheAction: ", myAction);
+            }
+            this.addRunAction("One");
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+            telemetry.addData("file not found e: ", e);
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            telemetry.addData("io exception: ", e);
+        }
+        finally
+        {
+            if(br != null)
+            {
+                try
+                {
+                    br.close();
+                }
+                catch(IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     /** The body of the code to execute: Creates a run list of actions to do, executes the current
